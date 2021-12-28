0edb82c6d950c6300d3307fa31ca7545a4015efa
vinicius.alexandre <vateiixeira@gmail.com>
2021-12-28 13:24:48 -0300

    oracle

--- my_project/core/conexao_oracle.py
+++ my_project/core/conexao_oracle.py
@@ -3,6 +3,15 @@ import time
 
 
 def conecta():
+    stats = {
+        'session_id':1,
+        'usuario':2,
+        'terminal':3,
+        'maquina':4,
+        'programa':5,
+        'os':6,
+        'sessao_bloqueada':7,
+    }
     uid="VILLE5"
     pwd="g00gl3"
     db="(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = villep-scan.villefort.lan)(PORT = 1521))(LOAD_BALANCE = YES)(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = CEMAPROD)(FAILOVER_MODE = (TYPE = SELECT)(METHOD = BASIC)(RETRIES = 180)(DELAY = 5))))"  # string de conexão do Oracle, configurado no
@@ -21,16 +30,40 @@ def conecta():
             dba_objects c 
             where b.sid = a.session_id and
             a.object_id = c.object_id and
-            blocking_session is not null
+            
             group by  b.inst_id,session_id ,b.osuser , terminal ,
             machine, program,os_user_name ,blocking_session,module
-            order by inst_id,session_id"""    )
-    result=cursor.fetchall()
+            order by inst_id,session_id""")
+    # blocking_session is not null
+    result = cursor.fetchall()
     cursor.close()
-    return result
+    print(type(result))
+    bloqueados = []
+    origem = []
+    origem_data = []
+    for i in result:
+        if i[stats['sessao_bloqueada']] != None:
+            bloqueados.append(i)
+            origem.append(i[stats['session_id']])
+        if i[stats['session_id']] in origem:
+            origem_data.append(i)
+        
+    return {
+        'bloqueados': bloqueados,
+        'origem': origem,
+        'origem_data': origem_data,
+    }
 
 
-
+def oracle_sessoes():
+    while True:
+        time.sleep(10)
+        data = conecta()
+        if len(data['bloqueados']) > 0:
+            print(data['bloqueados'])
+            print(data['origem'])
+            print(data['origem_data'])
+            print('-'*50)
 
 def convert(mes):
     if mes == 1:
